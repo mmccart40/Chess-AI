26ed19a3dfff32458ac5ed48a9da3344f64dc8fe
Thomas Held <theld144@gmail.com>
2024-11-07 10:37:34 -0500

    super dumb eval function

--- rookNRollers_ChessPlayer.py
+++ rookNRollers_ChessPlayer.py
@@ -9,5 +9,27 @@ class rookNRollers_ChessPlayer(ChessPlayer):
     def get_move(self, your_remaining_time, opp_remaining_time, prog_stuff):
         # YOUR MIND-BOGGLING CODE GOES HERE
         # (random choice for now)
+        print("Evaluation before move:", self.eval_function())
         return random.choice(
             self.board.get_all_available_legal_moves(self.color))
+
+    def eval_function(self):
+        
+		# set opponent's color
+        opponent = 'black' if self.color == 'white' else 'white'
+
+        if self.board.is_king_in_checkmate(self.color):
+            # if we are in checkmate, return -100
+            return -100
+        elif self.board.is_king_in_check(self.color):
+            # if we are in check, return -20
+            return -20
+        
+        if self.board.is_king_in_checkmate(opponent):
+            # if opponent is in checkmate, return 5
+            return 5
+        elif self.board.is_king_in_check(opponent):
+            # if opponent is in check, return 10
+            return 10
+        
+        return 0
